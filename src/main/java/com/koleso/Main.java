b8155df1393d305e7d1cfb1b796def5c376e7e57
hiflay <hiflay@mail.ru>
2026-05-18 17:56:22 +1000

    Add custom string builder

--- src/main/java/com/koleso/Main.java
+++ src/main/java/com/koleso/Main.java
@@ -1,6 +1,32 @@
 package com.koleso;
 
+import com.koleso.java.core.MyStringBuilder;
+
 public class Main {
     public static void main(String[] args) {
+        MyStringBuilder msb = new MyStringBuilder();
+
+        System.out.println(msb);
+
+        msb.append('a');
+        System.out.println(msb);
+
+        msb.append('b');
+        System.out.println(msb);
+
+        msb.append("test");
+        System.out.println(msb);
+
+        msb.undo();
+        System.out.println(msb);
+
+        msb.undo();
+        System.out.println(msb);
+
+        msb.undo();
+        System.out.println(msb);
+
+        msb.undo();
+        System.out.println(msb);
     }
 }

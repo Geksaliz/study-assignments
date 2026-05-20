4b329b1dea67fb38597e27c7d79a333adfec0e87
hiflay <hiflay@mail.ru>
2026-05-20 14:04:54 +1000

    Add collection counter

--- src/main/java/com/koleso/Main.java
+++ src/main/java/com/koleso/Main.java
@@ -1,6 +1,11 @@
 package com.koleso;
 
+import com.koleso.java.collection.CollectionCounter;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(CollectionCounter.count(List.of("1", "2", "2", "3", "3", "3", "4", "4", "4", "4")));
     }
 }

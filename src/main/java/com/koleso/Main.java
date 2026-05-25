36705425ddc5a41a780d00b495eeedb226a60a0d
hiflay <hiflay@mail.ru>
2026-05-25 21:05:45 +1000

    Clear main method

--- src/main/java/com/koleso/Main.java
+++ src/main/java/com/koleso/Main.java
@@ -1,11 +1,6 @@
 package com.koleso;
 
-import com.koleso.java.collection.CollectionCounter;
-
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(CollectionCounter.count(List.of("1", "2", "2", "3", "3", "3", "4", "4", "4", "4")));
     }
 }

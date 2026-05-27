9278cc7282b2701b13798e78d011bf351599341b
hiflay <hiflay@mail.ru>
2026-05-27 17:08:57 +1000

    Clear main method

--- src/main/java/com/koleso/Main.java
+++ src/main/java/com/koleso/Main.java
@@ -1,11 +1,6 @@
 package com.koleso;
 
-import com.koleso.java.concurrency.aggregator.DataAggregator;
-
 public class Main {
     public static void main(String[] args) {
-        DataAggregator dataAggregator = new DataAggregator();
-
-        System.out.println(dataAggregator.aggregateProductInfo("Ноутбук"));
     }
 }
